d5540c5cd2699d14630e48d26de985e281baee96
Simon Tharby <simontharby@gmail.com>
2024-01-12 11:48:10 -0300

    do not use nightly build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// #![feature(portable_simd)]
+// use core::simd::prelude::*;
+
 fn main() {
+    let vec_of_vec3s = vec![
+        vec![1.1, 2.2, 3.3],
+        vec![4.4, 5.5, 6.6],
+        vec![7.7, 8.8, 9.9]
+    ];
+
+    for vec3 in vec_of_vec3s {
+        for i in vec3 {
+            println!("{}", i);
+        }
+    }
+
     println!("Hello, world!");
 }
