03bce67dea6e59fec92483ec9a2b1be841417869
Simon Tharby <simontharby@gmail.com>
2024-01-12 16:18:50 -0300

    libnoise example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-// #![feature(portable_simd)]
-// use core::simd::prelude::*;
+use libnoise::*;
+use std::time::Instant;
 
 fn main() {
-    let vec_of_vec3s = vec![
-        vec![1.1, 2.2, 3.3],
-        vec![4.4, 5.5, 6.6],
-        vec![7.7, 8.8, 9.9]
-    ];
+    // res 512 (+ border 3) = 268_324 points
+    // res 1024 (+ border 3) = 1_060_900 points
+    // res 2048 (+ border 3) = 4_218_916 points
+    let mut vec_of_vec3s: Vec<Vec<f64>> = vec![];
 
-    for vec3 in vec_of_vec3s {
-        for i in vec3 {
-            println!("{}", i);
-        }
+    for _i in 0..4218915 {
+        vec_of_vec3s.push(vec![1.1, 2.2, 3.3]);
     }
 
-    println!("Hello, world!");
+    let start_time = Instant::now();
+
+    // let mut noise_data: Vec<f64> = Vec::new();
+
+    let octaves = 6;
+    let frequency = 1.0;
+    let lacunarity = 2.0;
+    let persistence = 0.5;
+
+    // build a simplex noise generator seeded with 42
+    let generator = Source::simplex(42).fbm(octaves, frequency, lacunarity, persistence);
+
+    // Slightly faster than for loop
+    let result = vec_of_vec3s.iter().map(|x| generator.sample([x[0], x[1], x[2]])).collect::<Vec<_>>();
+
+    let elapsed = start_time.elapsed();
+    println!("{:?}", result[42]);
+    println!("libnoise {:?}", elapsed);
+
+    // TODO: Add other noise library examples for comparison
 }
+
